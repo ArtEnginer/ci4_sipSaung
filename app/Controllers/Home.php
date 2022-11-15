1b0c13e11fe2868174480da3bcd40220246a139b
Angga Saputra <72662866+ArtEnginer@users.noreply.github.com>
2022-11-15 10:36:43 +0700

    landin done'

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,20 +4,28 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\JeniskelasModel as jenisKelasModel;
+use App\Models\TentangModel as tentangModel;
+
 class Home extends BaseController
 {
     public function __construct()
     {
         $this->data['config'] = config('Theme');
         $this->jeniskelasModel = new jenisKelasModel();
-
+        $this->tentangModel = new tentangModel();
     }
-   
+
     public function index()
-    {    
+    {
+        $this->data['jenisKelas'] = $this->jeniskelasModel->findAll();
+        $this->data['tentang'] = $this->tentangModel->first();
+        $this->data['item'] = $this->tentangModel->first();
+        $this->data['alamat'] = json_decode($this->data['item']->alamat);
+        $this->data['contact'] = json_decode($this->data['item']->contact);
+        $this->data['sosmed'] = json_decode($this->data['item']->sosmed);
         return view('App\Views\Frontend\Page\index', $this->data);
     }
-    
+
     public function pendaftaran()
     {
         $this->data['jenisKelas'] = $this->jeniskelasModel->findAll();
--- app/Views/Frontend/Page/index.php
+++ app/Views/Frontend/Page/index.php
@@ -17,17 +17,17 @@
 
                 <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
                     <div class="content">
-                        <h3>Who We Are</h3>
-                        <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>
+                        <h2><?= $tentang->judul ?></h2>
+                        <!-- <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2> -->
                         <p>
-                            Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor consequatur itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est corrupti.
+                            <?= $tentang->deskripsi ?>
                         </p>
-                        <div class="text-center text-lg-start">
+                        <!-- <div class="text-center text-lg-start">
                             <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
                                 <span>Read More</span>
                                 <i class="bi bi-arrow-right"></i>
                             </a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
 
@@ -46,35 +46,24 @@
         <div class="container" data-aos="fade-up">
 
             <header class="section-header">
-                <h2>Our Values</h2>
-                <p>Odit est perspiciatis laborum et dicta</p>
+                <h2>Jenis Kelas</h2>
+                <p>Banyak Pilihan Kelas yang bisa kamu pakai</p>
             </header>
 
             <div class="row">
 
-                <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
-                    <div class="box">
-                        <img src="<?= base_url('assets') ?>/img/values-1.png" class="img-fluid" alt="">
-                        <h3>Ad cupiditate sed est odio</h3>
-                        <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
+                <?php
+                $n = 1;
+                foreach ($jenisKelas as $kelas) :
+                ?>
+                    <div class="col-lg-4">
+                        <div class="box" data-aos="fade-up" data-aos-delay="200">
+                            <img src="<?= base_url('assets') . '/img/values-' . $n++ ?>.png" class="img-fluid" alt="">
+                            <h3><?= $kelas->nama ?></h3>
+                            <p><?= $kelas->deskripsi ?></p>
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
-                    <div class="box">
-                        <img src="<?= base_url('assets') ?>/img/values-2.png" class="img-fluid" alt="">
-                        <h3>Voluptatem voluptatum alias</h3>
-                        <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
-                    <div class="box">
-                        <img src="<?= base_url('assets') ?>/img/values-3.png" class="img-fluid" alt="">
-                        <h3>Fugit cupiditate alias nobis.</h3>
-                        <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
-                    </div>
-                </div>
+                <?php endforeach; ?>
 
             </div>
 
@@ -242,106 +231,6 @@
 
     </section><!-- End F.A.Q Section -->
 
-    <!-- ======= Testimonials Section ======= -->
-    <section id="testimonials" class="testimonials">
-
-        <div class="container" data-aos="fade-up">
-
-            <header class="section-header">
-                <h2>Testimonials</h2>
-                <p>What they are saying about us</p>
-            </header>
-
-            <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="200">
-                <div class="swiper-wrapper">
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                            </div>
-                            <p>
-                                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                            </p>
-                            <div class="profile mt-auto">
-                                <img src="<?= base_url('assets') ?>/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                                <h3>Saul Goodman</h3>
-                                <h4>Ceo &amp; Founder</h4>
-                            </div>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                            </div>
-                            <p>
-                                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                            </p>
-                            <div class="profile mt-auto">
-                                <img src="<?= base_url('assets') ?>/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                                <h3>Sara Wilsson</h3>
-                                <h4>Designer</h4>
-                            </div>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                            </div>
-                            <p>
-                                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                            </p>
-                            <div class="profile mt-auto">
-                                <img src="<?= base_url('assets') ?>/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                                <h3>Jena Karlis</h3>
-                                <h4>Store Owner</h4>
-                            </div>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                            </div>
-                            <p>
-                                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                            </p>
-                            <div class="profile mt-auto">
-                                <img src="<?= base_url('assets') ?>/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                                <h3>Matt Brandon</h3>
-                                <h4>Freelancer</h4>
-                            </div>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                            </div>
-                            <p>
-                                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                            </p>
-                            <div class="profile mt-auto">
-                                <img src="<?= base_url('assets') ?>/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                                <h3>John Larson</h3>
-                                <h4>Entrepreneur</h4>
-                            </div>
-                        </div>
-                    </div><!-- End testimonial item -->
-
-                </div>
-                <div class="swiper-pagination"></div>
-            </div>
-
-        </div>
-
-    </section><!-- End Testimonials Section -->
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
@@ -355,42 +244,40 @@
 
             <div class="row gy-4">
 
-                <div class="col-lg-6">
+                <div class="col-lg-12">
 
                     <div class="row gy-4">
+                        <?php
+                        foreach ($contact as $c) : ?>
+                            <div class="col-md-3">
+                                <div class="info-box">
+
+                                    <h3><?= $c->jenis ?></h3>
+                                    <p><?= $c->isi ?></p>
+                                </div>
+                            </div>
+                        <?php endforeach ?>
                         <div class="col-md-6">
                             <div class="info-box">
-                                <i class="bi bi-geo-alt"></i>
-                                <h3>Address</h3>
-                                <p>A108 Adam Street,<br>New York, NY 535022</p>
+                                <?php foreach ($alamat as $a) : ?>
+                                    <h3><?= $a->jenis ?></h3>
+                                    <p><?= $a->isi ?></p>
+                                <?php endforeach ?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="info-box">
-                                <i class="bi bi-telephone"></i>
-                                <h3>Call Us</h3>
-                                <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-envelope"></i>
-                                <h3>Email Us</h3>
-                                <p>info@example.com<br>contact@example.com</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-clock"></i>
-                                <h3>Open Hours</h3>
-                                <p>Monday - Friday<br>9:00AM - 05:00PM</p>
+                                <?php foreach ($sosmed as $s) : ?>
+                                    <h3><?= $s->jenis ?></h3>
+                                    <p><?= $s->isi ?></p>
+                                <?php endforeach ?>
                             </div>
                         </div>
                     </div>
 
                 </div>
 
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <form action="forms/contact.php" method="post" class="php-email-form">
                         <div class="row gy-4">
 
